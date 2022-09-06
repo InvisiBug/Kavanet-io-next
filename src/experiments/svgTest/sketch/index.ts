11281490dd15161930a149faae6703e77e9fbc12
InvisiBug <matthewjkavanagh@gmail.com>
2022-09-06 21:57:48 +0100

    Wip

--- src/experiments/svgTest/sketch/index.ts
+++ src/experiments/svgTest/sketch/index.ts
@@ -1,3 +1,5 @@
+// This repo has some good SVG uses
+// https://github.com/xanderjl/p5
 import p5 from "p5";
 
 export const sketch = (p5: p5) => {
@@ -8,26 +10,30 @@ export const sketch = (p5: p5) => {
   // const x = init(config.p5);
 
   p5.setup = () => {
+    p5.background(255);
     // @ts-ignore
-    p5.createCanvas(p5.windowWidth, p5.windowHeight, p5.SVG);
+    p5.createCanvas(p5.windowWidth, p5.windowHeight);
   };
 
   p5.draw = () => {
-    if (p5.frameCount > 100) {
+    if (p5.frameCount > 500) {
       p5.noLoop();
-      // p5.save();
-      // @ts-ignore
-      p5.saveSVG("mySVG.svg");
+      //   // p5.save();
+      //   // @ts-ignore
+      // p5.saveSVG("mySVG.svg");
+      p5.save();
     }
     // p5.background(50);
   };
 
-  p5.mouseClicked = () => {};
+  p5.mouseDragged = () => {};
 
-  p5.mouseDragged = () => {
-    p5.fill(255);
-    p5.noStroke();
-    p5.ellipse(p5.mouseX, p5.mouseY, 10);
+  p5.mouseClicked = () => {
+    p5.fill(0);
+    p5.stroke(0);
+    p5.strokeWeight(2);
+    // p5.ellipse(p5.mouseX, p5.mouseY, 10);
+    p5.line(0, 0, p5.mouseX, p5.mouseY);
   };
 };
 
