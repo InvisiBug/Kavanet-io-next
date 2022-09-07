80f7ffa2d07395df3a65190a92e8e26ecc0ed5cd
InvisiBug <matthewjkavanagh@gmail.com>
2022-09-07 23:09:07 +0100

    Need to get weighting working correctly

--- src/experiments/vertex/sketch/index.ts
+++ src/experiments/vertex/sketch/index.ts
@@ -5,34 +5,39 @@ export const sketch = (p5: p5) => {
     p5,
   };
 
-  const numRows = 10;
-  const numCols = 10;
-  const squareSize = 75;
-
-  const xpoints = 10;
-  const ypoints = 10;
-  const margin = 200;
+  const numRows = 20;
+  const squareSize = p5.windowHeight / numRows;
+  const numCols = Math.floor(p5.windowWidth / squareSize);
 
   const xstart = p5.windowWidth / 2 - (squareSize * numCols) / 2;
-  const ystart = p5.windowHeight / 2 - (squareSize * numRows) / 2;
+  // const ystart = p5.windowHeight / 2 - (squareSize * numRows) / 2;
+  // const xstart = 0;
+  const ystart = 0;
 
-  const colours = ["#302F2D", "#464543", "#274666", "#24829F", "#86A95B"];
+  // const colours = ["#302F2D", "#464543", "#274666", "#24829F", "#86A95B"];
+  const colours = ["#59BA92", "#0E3556", "#1F5545", "#8284A4", "#F25E40"];
+  // const colours = ["#86A95B"];
   let vertex: number[][] = [];
 
   p5.setup = () => {
     p5.createCanvas(p5.windowWidth, p5.windowHeight);
+    p5.noLoop();
   };
 
   p5.draw = () => {
     p5.background(50);
-    p5.noLoop();
+    // drawBackground();
 
-    for (let x = 0; x < xpoints; x++) {
-      for (let y = 0; y < ypoints; y++) {
+    for (let x = 0; x < numCols; x++) {
+      for (let y = 0; y < numRows; y++) {
         p5.noStroke();
+        p5.fill(colours[0]);
+        p5.fill(p5.random(colours));
 
-        pickColour(p5, colours);
+        // pickColour(p5, colours);
+        chooseWaitedColour(x, y);
 
+        // Top
         p5.triangle(
           xstart + x * squareSize,
           ystart + y * squareSize,
@@ -42,8 +47,10 @@ export const sketch = (p5: p5) => {
           ystart + (y + 1) * squareSize
         );
 
-        pickColour(p5, colours);
+        // pickColour(p5, colours);
+        // chooseWaitedColour(x, y);
 
+        // Bottom
         p5.triangle(
           xstart + (x + 1) * squareSize,
           ystart + y * squareSize,
@@ -56,15 +63,50 @@ export const sketch = (p5: p5) => {
     }
   };
 
+  const drawBackground = () => {
+    p5.noStroke();
+
+    const backgroundColours = ["#13191B", "#F3F3F3"];
+
+    p5.fill(backgroundColours[0]);
+    p5.triangle(0, 0, p5.windowWidth, 0, 0, p5.windowHeight);
+
+    p5.fill(backgroundColours[1]);
+    p5.triangle(p5.windowWidth, p5.windowHeight, p5.windowWidth, 0, 0, p5.windowHeight);
+  };
+
   p5.mouseClicked = () => {};
 
   p5.mouseDragged = () => {};
+
+  const chooseWaitedColour = (x: number, y: number) => {
+    const xweight = p5.map(x, 0, numCols - 1, 0, 0.9);
+    const yweight = p5.map(y, 0, numRows - 1, 0, 0.9);
+
+    // if (p5.random() >= xweight * 0.1 && p5.random() >= yweight) {
+
+    if (p5.random() >= xweight) {
+      if (p5.random() >= yweight) {
+        p5.fill(50);
+      }
+    }
+
+    // if (p5.random() >= 1 - xweight / 0.75) {
+    //   if (p5.random() >= 1 - yweight / 1) {
+    //     p5.fill(50);
+    //   }
+    // }
+
+    //? Need to figure out how to stop them appearing in top left and bottom right
+    // https://stackoverflow.com/questions/13300904/determine-whether-point-lies-inside-triangle
+
+    // console.log(`x ${xweight.toFixed(2)}\ty ${yweight.toFixed(2)}`);
+  };
 };
 
 const pickColour = (p5: p5, colours: string[]) => {
   const randomVal = p5.random();
 
-  console.log(randomVal);
   if (randomVal <= 0.9) {
     p5.fill(p5.random(colours));
   } else {
