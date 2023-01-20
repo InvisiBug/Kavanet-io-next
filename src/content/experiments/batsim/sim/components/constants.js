3d99a8e8e1454143adac2e19547f50c82a13f82b
InvisiBug <matthewjkavanagh@gmail.com>
2023-01-20 16:03:26 +0000

    Moved a bunch of sketches to my sketchbook

--- src/content/experiments/batsim/sim/components/constants.js
+++
@@ -1,3 +0,0 @@
-export const woodland = 1;
-export const woodlandEdge = 2;
-export const buildings = 3;
--- src/content/experiments/lightning/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ReactP5Wrapper } from "react-p5-wrapper";
-import { sketch } from "./sketch";
-
-const Experiment = () => {
-  return <ReactP5Wrapper sketch={sketch} />;
-};
-
-export default Experiment;
