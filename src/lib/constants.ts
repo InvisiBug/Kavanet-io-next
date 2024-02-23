c79ab6b1f6cd0ad7e5705925e457bd5b89eba472
InvisiBug <matthewjkavanagh@gmail.com>
2024-02-23 23:44:28 +0000

    removed plots from nav bar

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,8 @@
 const local = process.env.NEXT_PUBLIC_LOCAL;
 console.log("🚀 ~ local:", local);
 
-export const links = ["StudioFree", "Projects", "Experiments", "Plots"];
+// export const links = ["StudioFree", "Projects", "Experiments", "Plots"];
+export const links = ["StudioFree", "Projects", "Experiments"];
 
 if (local === "true") {
   links.unshift("WIP");
